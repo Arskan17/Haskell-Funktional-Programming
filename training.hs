8337fcf665c05b6e856455e7fec51136712edb43
Kali <kali@kali>
2022-12-08 15:22:27 -0500

    training

--- training.hs
+++ training.hs
@@ -1,8 +1,11 @@
 module Foo where
+import Data.Char
 
+-- this applies a fxn to all elements of a list, and the sums up the list
 accum f [] = f []
 accum f (x:xs) = f x + accum f xs
 
+-- this concats a list of lists in to a list, applies a fx to all its elems and sums em' all up 
 accum2 f (xs:xss) = f (head zs) + accum f zs
                     where
                         zs = xs ++ concat xss
@@ -10,13 +13,16 @@ accum2 f (xs:xss) = f (head zs) + accum f zs
 
 data Tree a = Leaf a | Node (Tree a) (Tree a)
 
+-- this applies a fxn to the elems in all the Leaves of a tree, and then sums em' all up
 accumt f (Leaf n) = f n
 accumt f (Node l r)  = accumt f l + accumt f r
 
+-- this applies a fxn to the sum of a list
 unk f [] = f 0
 unk f (x:xs) = unk (\n -> f (n+x)) xs
 -- unk f (x:xs) = f x + unk f xs
 
+-- generates a tree of a givensize n
 gen :: Int -> [Tree a]
 gen 0 = []
 -- gen 1 = [Leaf ]
@@ -25,5 +31,16 @@ gen n = [Node l r | k <- [0..nm],j <- [nm+1..n-1], l <- gen k, r <- gen j]
             where
                 nm = div n 2
 
-siev = [a |  [x | x<-[2..]] ]
-        
+-- gives back the infinite list of prime numbers
+primes = sieve [2..]
+-- uses the sieve of Erathostenes to calculate the numbers
+sieve (p:xs) = p:sieve [a | a <- xs, a `mod` p /= 0] 
+
+-- checks if a tupple of two prime numbers are twins. twins are prime numbers which only differ by two
+twin (x,y) = y == x+2
+-- gives out the list of tupples of all twin prime numbers. Note that it has yet to be proven, whether this list is infinite or not!
+twins = filter twin (zip primes (tail primes))
+
+-- checks if two lists, more specifically lists of characters are equal, whether they're the same letter case or not. 
+eqls xs ys = if length xs == length ys then (and [toLower (xs!!i) == ys!!i || toLower (ys!!i) == xs!!i || xs!!i == ys!!i |  i<-[0..(length xs)-1]])
+                else False
